620a16013539ed8f30409fabded302078748abd0
Tawanda Andrew Msengezi <xrthemc@gmail.com>
2026-02-25 17:57:13 +0200

    Add JavaScript Solution for 2026 Winter Games Day 1: Opening Day problem

---
+++ 2026 Winter Games Day 1 Opening Day/index.js
@@ -0,0 +1,245 @@
+function getFlag(code) {
+  switch(code){
+    case "AL":
+      return "🇦🇱";
+    case "AD":
+      return "🇦🇩";
+    case "AR":
+      return "🇦🇷";
+    case "AM":
+      return "🇦🇲";
+    case "AU":
+      return "🇦🇺";
+    case "AT":
+      return "🇦🇹";
+    case "AZ":
+      return "🇦🇿";
+    case "BE":
+      return "🇧🇪";
+    case "BJ":
+      return "🇧🇯";
+    case "BO":
+      return "🇧🇴";
+    case "BA":
+      return "🇧🇦";
+    case "BR":
+      return "🇧🇷";
+    case "BG":
+      return "🇧🇬";
+    case "CA":
+      return "🇨🇦";
+    case "CL":
+      return "🇨🇱";
+    case "CN":
+      return "🇨🇳";
+    case "CO":
+      return "🇨🇴";
+    case "HR":
+      return "🇭🇷";
+    case "CY":
+      return "🇨🇾";
+    case "CZ":
+      return "🇨🇿";
+    case "DK":
+      return "🇩🇰";
+    case "EC":
+      return "🇪🇨";
+    case "ER":
+      return "🇪🇷";
+    case "EE":
+      return "🇪🇪";
+    case "FI":
+      return "🇫🇮";
+    case "FR":
+      return "🇫🇷";
+    case "GE":
+      return "🇬🇪";
+    case "DE":
+      return "🇩🇪";
+    case "GB":
+      return "🇬🇧";
+    case "GR":
+      return "🇬🇷";
+    case "GW":
+      return "🇬🇼";
+    case "HT":
+      return "🇭🇹";
+    case "HK":
+      return "🇭🇰";
+    case "HU":
+      return "🇭🇺";
+    case "IS":
+      return "🇮🇸";
+    case "IS":
+      return "🇮🇸";
+    case "IN":
+      return "🇮🇳";
+    case "IR":
+      return "🇮🇷";
+    case "IE":
+      return "🇮🇪";
+    case "IL":
+      return "🇮🇱";
+    case "IT":
+      return "🇮🇹";
+    case "JM":
+      return "🇯🇲";
+    case "JP":
+      return "🇯🇵";
+    case "KZ":
+      return "🇰🇿";
+    case "KE":
+      return "🇰🇪";
+    case "XK":
+      return "🇽🇰";
+    case "KG":
+      return "🇰🇬";
+    case "LV":
+      return "🇱🇻";
+    case "LB":
+      return "🇱🇧";
+    case "LI":
+      return "🇱🇮";
+    case "LT":
+      return "🇱🇹";
+    case "LU":
+      return "🇱🇺";
+    case "MG":
+      return "🇲🇬";
+    case "MY":
+      return "🇲🇾";
+    case "MT":
+      return "🇲🇹";
+    case "MX":
+      return "🇲🇽";
+    case "MD":
+      return "🇲🇩";
+    case "MC":
+      return "🇲🇨";
+    case "MN":
+      return "🇲🇳";
+    case "ME":
+      return "🇲🇪";
+    case "MA":
+    	return "🇲🇦";
+    case "NL":
+      return "🇳🇱";
+    case "NZ":
+      return "🇳🇿";
+    case "NG":
+      return "🇳🇬";
+    case "MK":
+      return "🇲🇰";
+    case "NO":
+      return "🇳🇴";
+    case "PK":
+      return "🇵🇰";
+    case "PH":
+      return "🇵🇭";
+    case "PL":
+      return "🇵🇱";
+    case "PT":
+      return "🇵🇹";
+    case "PR":
+      return "🇵🇷";
+    case "RO":
+      return "🇷🇴";
+    case "SM":
+      return "🇸🇲";
+    case "SA":
+      return "🇸🇦";
+    case "RS":
+      return "🇷🇸";
+    case "SG":
+      return "🇸🇬";
+    case "SK":
+      return "🇸🇰";
+    case "SI":
+      return "🇸🇮";
+    case "ZA":
+      return "🇿🇦";
+    case "KR":
+      return "🇰🇷";
+    case "ES":
+     return "🇪🇸";
+    case "SE":
+     return "🇸🇪";
+    case "CH":
+     return "🇨🇭";
+    case "TH":
+     return "🇹🇭";
+    case "TT":
+     return "🇹🇹";
+    case "TR":
+     return "🇹🇷";
+    case "UA":
+     return "🇺🇦";
+    case "AE":
+     return "🇦🇪";
+    case "US":
+     return "🇺🇸";
+    case "UY":
+     return "🇺🇾";
+    case "UZ":
+     return "🇺🇿";
+    case "VE":
+     return "🇻🇪";
+  }
+}
+
+// Tests
+console.log(getFlag("AL"))
+console.log(getFlag("AD"))
+console.log(getFlag("AR"))
+console.log(getFlag("AM"))
+console.log(getFlag("AU"))
+console.log(getFlag("AT"))
+console.log(getFlag("AZ"))
+console.log(getFlag("BE"))
+console.log(getFlag("BO"))
+console.log(getFlag("BA"))
+console.log(getFlag("BR"))
+console.log(getFlag("BG"))
+console.log(getFlag("CA"))
+console.log(getFlag("CL"))
+console.log(getFlag("CN"))
+console.log(getFlag("CO"))
+console.log(getFlag("HR"))
+console.log(getFlag("CY"))
+console.log(getFlag("DK"))
+console.log(getFlag("EC"))
+console.log(getFlag("ER"))
+console.log(getFlag("EE"))
+console.log(getFlag("FI"))
+console.log(getFlag("FR"))
+console.log(getFlag("GE"))
+console.log(getFlag("DE"))
+console.log(getFlag("GB"))
+console.log(getFlag("GR"))
+console.log(getFlag("GW"))
+console.log(getFlag("HT"))
+console.log(getFlag("HK"))
+console.log(getFlag("HU"))
+console.log(getFlag("IS"))
+console.log(getFlag("IN"))
+console.log(getFlag("IR"))
+console.log(getFlag("IE"))
+console.log(getFlag("IL"))
+console.log(getFlag("IT"))
+console.log(getFlag("JM"))
+console.log(getFlag("JP"))
+console.log(getFlag("KZ"))
+console.log(getFlag("KE"))
+console.log(getFlag("XK"))
+console.log(getFlag("KG"))
+console.log(getFlag("LV"))
+console.log(getFlag("LB"))
+console.log(getFlag("LI"))
+console.log(getFlag("LT"))
+console.log(getFlag("LU"))
+console.log(getFlag("MG"))
+console.log(getFlag("MY"))
+console.log(getFlag("MT"))
+console.log(getFlag("MX"))
+console.log(getFlag("MD"))
+console.log(getFlag("MC"))
